6a07ae0d494fb2db1aafcc76876141f349d7c917
Marsella, Marco (CBDT) <Marco.Marsella@fao.org>
2019-02-18 16:52:47 +0100

    Adjusted a few things and successfully tested

--- src/main/java/glis/toolkit/Main.java
+++ src/main/java/glis/toolkit/Main.java
@@ -4,7 +4,6 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
-import org.apache.http.HttpException;
 import org.apache.http.HttpStatus;
 import org.joox.Match;
 import org.sql2o.Connection;
@@ -13,8 +12,10 @@ import org.sql2o.Sql2o;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.text.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -23,14 +24,14 @@ import static org.joox.JOOX.$;
 
 public class Main {
 
-    public static final String CONFIG_PATH = "config.txt";
-    public static final String TIMESTAMP   = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+    private static final String CONFIG_PATH = "config.txt";
+	private static final String TIMESTAMP   = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
 
-	public static String  glisUrl;
-	public static String  glisUsername;
-	public static String  glisPassword;
-	public static Integer qlimit;
-	public static Sql2o   sql2o;
+	private static String  glisUrl;
+	private static String  glisUsername;
+	private static String  glisPassword;
+	private static Integer qlimit;
+	private static Sql2o   sql2o;
 
     public static void main(String[] args) {
         
@@ -68,22 +69,21 @@ public class Main {
 			System.out.println("GLIS password:     [" + glisPassword + "]");
 			
 			// Register first then update
-			process("register");
-			process("update");
+			process("register", fDOI);
+			process("update", fDOI);
+
+			// remove lock file
+			fLock.delete();
 		}
 		catch (Exception e) {
             e.printStackTrace();
 		}
-		finally {
-			// remove lock file
-			fLock.delete();
-		}
     }
 
-	private static void process(String operation) throws Exception {
+	private static void process(String operation, Writer fDOI) throws Exception {
 
 		// build list of pgrfa ids to register
-		List<String> ids = select(sql2o, conn ->
+		List<String> ids = select(conn ->
 				conn.createQuery("select id from pgrfas where operation=:operation and processed=:processed")
 						.addParameter("operation", operation)
 						.addParameter("processed", "n"))
@@ -93,13 +93,12 @@ public class Main {
 				.collect(Collectors.toList());
 
 		// process each pgrfa
-		Integer count = 0;
         Map<String, Object> conf = new HashMap<> ();
 		conf.put("glis_username",glisUsername);
 		conf.put("glis_password",glisPassword);
 
 		for (String id: ids) {
-			Map<String, Object> pgrfa = select(sql2o, conn -> conn.createQuery("select * from pgrfas where id=:id").addParameter("id", id)).get(0);
+			Map<String, Object> pgrfa = select(conn -> conn.createQuery("select * from pgrfas where id=:id").addParameter("id", id)).get(0);
 
 			String   wiews      = pgrfa.get("hold_wiews").toString();
 			String   pid        = pgrfa.get("hold_pid").toString();
@@ -115,31 +114,40 @@ public class Main {
 					.asString();
 			String   xmlResponse = httpResponse.getBody();
 			Document response = $(xmlResponse).document();
-			if (httpResponse.getStatus() == HttpStatus.SC_OK) {
 
-				// do something...
-			}
-			else {
-				String sampleId = $(response).child("sampleid").text();
-				String genus    = $(response).child("genus").text();
-				String error    = $(response).child("error").text();
+			//Extract relevant information, if available
+			String doi      = $(response).child("doi").text();
+			String sampleId = $(response).child("sampleid").text();
+			String genus    = $(response).child("genus").text();
+			String error    = $(response).child("error").text();
 
+			//System.out.println(genus + "\t" + sampleId + "\t" + doi + "\t" + error);
+
+			String result   = ((error != null) && (!error.isEmpty())) ? "KO" : "OK";
+
+			if (httpResponse.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
 				System.err.println(sampleId + " - " + genus + " - " + error);
-
-				throw new HttpException(httpResponse.getStatus() + " " + httpResponse.getStatusText());
+				System.exit(1);
 			}
+			if (Objects.equals(result,"OK")) {
+				fDOI.write(wiews + "\t" + pid + "\t" + genus + "\t" + sampleId + "\t" + doi + "\n");
+			}
+			// Write result to DB
+			insertResult(operation, result, doi, sampleId, genus, error);
+			markAsProcessed(id);
+			System.out.println("Processed sample [" + sampleId + "]: " + result);
 		}
 	}
 
 	private static Document buildDocument(String id, Map<String, Object> pgrfa, Map<String, Object> conf) throws ParserConfigurationException {
 
 		// get related tables
-		List<Map<String, Object>> actors      = select(sql2o, conn -> conn.createQuery("select * from actors      where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
-		List<Map<String, Object>> identifiers = select(sql2o, conn -> conn.createQuery("select * from identifiers where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
-		List<Map<String, Object>> names       = select(sql2o, conn -> conn.createQuery("select * from names       where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
-		List<Map<String, Object>> progdois    = select(sql2o, conn -> conn.createQuery("select * from progdois    where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
-		List<Map<String, Object>> targets     = select(sql2o, conn -> conn.createQuery("select * from targets     where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
-		List<Map<String, Object>> tkws        = select(sql2o, conn -> conn.createQuery("select k.* from tkws k, targets t where t.pgrfa_id=:pgrfa_id and t.id=k.target_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> actors      = select(conn -> conn.createQuery("select * from actors      where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> identifiers = select(conn -> conn.createQuery("select * from identifiers where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> names       = select(conn -> conn.createQuery("select * from names       where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> progdois    = select(conn -> conn.createQuery("select * from progdois    where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> targets     = select(conn -> conn.createQuery("select * from targets     where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
+		List<Map<String, Object>> tkws        = select(conn -> conn.createQuery("select k.* from tkws k, targets t where t.pgrfa_id=:pgrfa_id and t.id=k.target_id").addParameter("pgrfa_id", id));
 
 		return $("root",
 				 addMap($("conf"),        conf),
@@ -159,12 +167,37 @@ public class Main {
 				.document();
 	}
 
-    // more readable than sql2o.withConnection...
-    private static List<Map<String, Object>> select(Sql2o sql2o, Function<Connection, Query> query) {
-        try (Connection conn = sql2o.open()) {
-            return query.apply(conn).executeAndFetchTable().asList();
-        }
-    }
+	// more readable than sql2o.withConnection...
+	private static void markAsProcessed(String id) {
+		final String query = "update pgrfas set processed = 'y' where id=:id";
+		try (Connection conn = sql2o.open()) {
+			conn.createQuery(query)
+					.addParameter("id", Integer.getInteger(id))
+					.executeUpdate();
+		}
+	}
+
+	// more readable than sql2o.withConnection...
+	private static void insertResult(String operation, String result, String doi, String sampleId, String genus, String error) {
+		final String query = "insert into results (operation, genus, sample_id, doi, result, error) values(:ope, :gen, :sid, :doi, :res, :err)";
+		try (Connection conn = sql2o.open()) {
+			conn.createQuery(query)
+					.addParameter("ope", operation)
+					.addParameter("gen", genus)
+					.addParameter("sid", sampleId)
+					.addParameter("doi", doi)
+					.addParameter("res", result)
+					.addParameter("err", error)
+					.executeUpdate();
+		}
+	}
+
+	// more readable than sql2o.withConnection...
+	private static List<Map<String, Object>> select(Function<Connection, Query> query) {
+		try (Connection conn = sql2o.open()) {
+			return query.apply(conn).executeAndFetchTable().asList();
+		}
+	}
 
 	private static Match addList(Match m, List<Map<String, Object>> list) {
 		list.forEach(map -> addMap(m, map));
