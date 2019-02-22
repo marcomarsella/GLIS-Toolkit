6b05d92e9c7238e0321908112f384aef8d07537a
Marsella, Marco (CBDT) <Marco.Marsella@fao.org>
2019-02-22 10:06:42 +0100

    Merged with development version

--- src/main/java/glis/toolkit/Main.java
+++ src/main/java/glis/toolkit/Main.java
@@ -12,9 +12,7 @@ import org.sql2o.Sql2o;
 import org.w3c.dom.Document;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.Writer;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Function;
@@ -33,58 +31,76 @@ public class Main {
 	private static Integer qlimit;
 	private static Sql2o   sql2o;
 
+	/*
+	 * Main method, sets up the environment and invokes registration and update functions
+	 */
     public static void main(String[] args) {
-        
-		File fLock = new File("lock.lck");
-		try (Writer fDOI = new FileWriter(TIMESTAMP + "_" + "DOI.txt")) {
 
-        	// file-based locking mechanism to ensure that only one instance is running at any given time
-			if (!fLock.exists()) {
-  				fLock.createNewFile();
+		try {
+			//Redirect standard error to file
+			PrintStream console = System.err;	// Save console for later use if required
+			File file = new File(TIMESTAMP + "_" + "errors.txt");
+			try {
+				FileOutputStream fos = new FileOutputStream(file);
+				PrintStream ps = new PrintStream(fos);
+				System.setErr(ps);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				System.exit(1);
 			}
-			else {
+
+			File fLock = new File("lock.lck");
+			// file-based locking mechanism to ensure that only one instance is running at any given time
+			if (!fLock.exists()) {
+				fLock.createNewFile();
+			} else {
 				System.out.println("Lock file 'lock.lck' exists. Either another instance of the Toolkit is running or it has terminated in an error. Please make sure no other instance of the Toolkit is running, or fix the error, before removing the lock file and trying again");
 				System.exit(1);
 			}
-            // read configuration
-            PropertiesConfiguration config   = new Configurations().properties(new File(CONFIG_PATH));
-            String                  url      = config.getString("db.url");
-            String                  username = config.getString("db.username");
-            String                  password = config.getString("db.password");
-            
-            sql2o        = new Sql2o(url, username, password);
-            glisUrl      = config.getString("glis.url");
-            qlimit       = Integer.parseInt(config.getString("db.query_limit"));
-            glisUsername = config.getString("glis.username");
-            glisPassword = config.getString("glis.password");
+			// read configuration
+			PropertiesConfiguration config = new Configurations().properties(new File(CONFIG_PATH));
+			String url = config.getString("db.url");
+			String username = config.getString("db.username");
+			String password = config.getString("db.password");
 
-			//Print configuration
-			System.out.println("Configuration");
-			System.out.println("Database URL:      [" + url + "]");
-			System.out.println("Database username: [" + username + "]");
-			System.out.println("Database password: [" + password + "]");
-			System.out.println("Query limit:       [" + qlimit + "]");
-			System.out.println("GLIS URL:          [" + glisUrl + "]");
-			System.out.println("GLIS username:     [" + glisUsername + "]");
-			System.out.println("GLIS password:     [" + glisPassword + "]");
-			
+			sql2o = new Sql2o(url, username, password);
+			glisUrl = config.getString("glis.url");
+			qlimit = Integer.parseInt(config.getString("db.query_limit"));
+			glisUsername = config.getString("glis.username");
+			glisPassword = config.getString("glis.password");
+
+			//Print configuration to both the console and to errors.txt
+			String configuration = "Configuration\n" +
+				"Database URL:      [" + url + "]\n" +
+				"Database username: [" + username + "]\n" +
+				"Database password: [" + password + "]\n" +
+				"Query limit:       [" + qlimit + "]\n" +
+				"GLIS URL:          [" + glisUrl + "]\n" +
+				"GLIS username:     [" + glisUsername + "]\n" +
+				"GLIS password:     [" + glisPassword + "]\n";
+			System.out.println(configuration);
+			System.err.println(configuration);
+
 			// Register first then update
-			process("register", fDOI);
-			process("update", fDOI);
+				process("register");
+				process("update");
 
-			// remove lock file
+			// Remove lock file and exit normally
 			fLock.delete();
-		}
-		catch (Exception e) {
-            e.printStackTrace();
+			System.out.println("Processing complete.");
+		} catch (Exception e) {
+			System.err.println(e.getStackTrace());
 		}
     }
 
-	private static void process(String operation, Writer fDOI) throws Exception {
+    /*
+     * Finds pgrfas rows to process and processes them according to the operation requested
+     */
+	private static void process(String operation) throws Exception {
 
-		// build list of pgrfa ids to register
+		// Build list of pgrfa ids to register. Order by id to ensure a logical sequence
 		List<String> ids = select(conn ->
-				conn.createQuery("select id from pgrfas where operation=:operation and processed=:processed")
+				conn.createQuery("select id from pgrfas where operation=:operation and processed=:processed order by id")
 						.addParameter("operation", operation)
 						.addParameter("processed", "n"))
 				.stream()
@@ -92,22 +108,43 @@ public class Main {
 				.map(m -> m.get("id").toString())
 				.collect(Collectors.toList());
 
-		// process each pgrfa
+		// Check if any row has been found with processed = 'n'
+		Integer idNum = ids.size();
+		if (idNum == 0) {
+			System.out.println("No pgrfas rows found to " + operation);
+			return;
+		} else {
+			System.out.println(ids.size() + " pgrfas rows found to " + operation);
+		}
+
+		// Build the config element to be added to the XML
         Map<String, Object> conf = new HashMap<> ();
 		conf.put("glis_username",glisUsername);
 		conf.put("glis_password",glisPassword);
 
-		int count = 0;
+		// Process each pgrfa in the list obtained by the query
+		Integer count = 0;
 		for (String id: ids) {
+
+			// Extract main pgrfa record
 			Map<String, Object> pgrfa = select(conn -> conn.createQuery("select * from pgrfas where id=:id").addParameter("id", id)).get(0);
 
+			// Save values to be used in response object
 			String   wiews      = pgrfa.get("hold_wiews").toString();
 			String   pid        = pgrfa.get("hold_pid").toString();
+
+			// Build the XML document
 			Document doc        = buildDocument(id, pgrfa, conf);
+			//DEBUG $(doc).write(new File("output.xml"));
+
+			// Transform XML using the XSL stylesheet and then transform again removing all empty elements
 			Document request    = transform(doc,     "transform.xsl");
+			//DEBUG $(doc).write(new File("transformed.xml"));
 			request             = transform(request, "prune.xsl");
+			//DEBUG $(doc).write(new File("pruned.xml"));
 			String   xmlRequest = $(request).toString();
 
+			// Attempts the HTTP POST transaction to GLIS and obtains result
 			try {
 				HttpResponse<String> httpResponse = Unirest.post(glisUrl)
 						.header("accept", "application/xml")
@@ -116,29 +153,34 @@ public class Main {
 				String   xmlResponse = httpResponse.getBody();
 				Document response = $(xmlResponse).document();
 
-				//Extract relevant information, if available
+				// Extract relevant information, if available
 				String doi      = $(response).child("doi").text();
 				String sampleId = $(response).child("sampleid").text();
 				String genus    = $(response).child("genus").text();
 				String error    = $(response).child("error").text();
+				//DEBUG System.out.println(genus + "\t" + sampleId + "\t" + doi + "\t" + error);
 
-				//System.out.println(genus + "\t" + sampleId + "\t" + doi + "\t" + error);
-
+				// Set the result of the transaction
 				String result   = ((error != null) && (!error.isEmpty())) ? "KO" : "OK";
 
+				// If there was a connection error, abort
 				if (httpResponse.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
 					System.err.println(sampleId + " - " + genus + " - " + error);
-					Unirest.shutdown();System.exit(1);
+					Unirest.shutdown();
+					System.exit(1);
 				}
-				if (Objects.equals(result,"OK")) {
-					fDOI.write(wiews + "\t" + pid + "\t" + genus + "\t" + sampleId + "\t" + doi + "\n");
-				}
-				// Write result to DB
+
+				// Write result to DB and mark pgrfas row as processed
 				insertResult(operation, result, doi, sampleId, genus, error);
 				markAsProcessed(id);
+
+				// Provide some feedback on the operation
 				count++;
-				System.out.println("Processed sample [" + count + "][" + sampleId + "]: " + result);
-			} catch (com.mashape.unirest.http.exceptions.UnirestException e) {
+				System.out.println("Processed pgrfa row [count: " + count.toString() + "/" + idNum + "] [id: " + id + "] [sample_id: " + sampleId + "]: " + result);
+			}
+			// Catch any exception and log it to the console and to the errors file
+			catch (com.mashape.unirest.http.exceptions.UnirestException e) {
+				System.out.println("Exception: " + e.getMessage());
 				System.err.println("Exception: " + e.getMessage());
 				Unirest.shutdown();
 				System.exit(1);
@@ -147,9 +189,12 @@ public class Main {
 		Unirest.shutdown();
 	}
 
+	/*
+	 * Builds the XML document by extracting the various rows related to the passed pgrfa
+	 */
 	private static Document buildDocument(String id, Map<String, Object> pgrfa, Map<String, Object> conf) throws ParserConfigurationException {
 
-		// get related tables
+		// Get related rowsets
 		List<Map<String, Object>> actors      = select(conn -> conn.createQuery("select * from actors      where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
 		List<Map<String, Object>> identifiers = select(conn -> conn.createQuery("select * from identifiers where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
 		List<Map<String, Object>> names       = select(conn -> conn.createQuery("select * from names       where pgrfa_id=:pgrfa_id").addParameter("pgrfa_id", id));
@@ -169,13 +214,18 @@ public class Main {
 		return result.document();
 	}
 
-	private static Document transform(Document doc, String xslPath) {
+	/*
+     * Transforms the XML document using the passed XSLT stylesheet
+     */
+    private static Document transform(Document doc, String xslPath) {
 		return $(doc)
 				.transform(Main.class.getClassLoader().getResourceAsStream(xslPath))
 				.document();
 	}
 
-	// more readable than sql2o.withConnection...
+	/*
+	 * Wrapper for the update operation to the pgrfas table
+	*/
 	private static void markAsProcessed(String id) {
 		final String query = "update pgrfas set processed = 'y' where id=:id";
 		try (Connection conn = sql2o.open()) {
@@ -185,7 +235,9 @@ public class Main {
 		}
 	}
 
-	// more readable than sql2o.withConnection...
+	/*
+	 * Wrapper for the insert operation into the results table
+	 */
 	private static void insertResult(String operation, String result, String doi, String sampleId, String genus, String error) {
 		final String query = "insert into results (operation, genus, sample_id, doi, result, error) values(:ope, :gen, :sid, :doi, :res, :err)";
 		try (Connection conn = sql2o.open()) {
@@ -200,23 +252,58 @@ public class Main {
 		}
 	}
 
-	// more readable than sql2o.withConnection...
+	/*
+	 * Wrapper for the select operation
+	 */
 	private static List<Map<String, Object>> select(Function<Connection, Query> query) {
 		try (Connection conn = sql2o.open()) {
 			return query.apply(conn).executeAndFetchTable().asList();
 		}
 	}
 
+	/*
+	 * Wrapper to add a list of maps to the XML document
+	 */
 	private static Match addList(Match m, String label, List<Map<String, Object>> list) {
 		list.forEach(map -> m.append(addMap(label, map)));
 		return m;
 	}
 
+	/*
+	 * Wrapper to add a map to the XML document
+	 */
 	private static Match addMap(String label, Map<String, Object> map) {
-    	Match m = $(label);
+		Match m = $(label);
 		map.entrySet().stream()
 				.filter(entry -> entry.getValue() != null)
 				.forEach(entry -> m.append($(entry.getKey()).text(entry.getValue().toString())));
 		return m;
+	}
+
+
+	/*
+	 * Contains some code that will become useful later on
+	 */
+	private static void dump() {
+		// Process target keywords by splitting the target.tkws field into a new list of maps
+		List<Map<String, Object>> targets = new ArrayList<Map<String, Object>>();	//Delete
+		List<Map<String, Object>> tkws = new ArrayList<Map<String, Object>>();
+		for (Map<String, Object> target : targets) {
+			String tid = target.get("id") + "";
+			Object tkwObj = target.get("tkws");
+			if (tkwObj != null) {
+				String tkwString = tkwObj.toString();
+				if ((tkwString != null) && (!tkwString.isEmpty())) {
+					String[] temp = tkwString.split("\\s*,\\s*");
+					for (String tkw : temp) {
+						Map<String, Object> tkmap = new HashMap<String, Object>();
+						tkmap.put("target_id", tid);
+						tkmap.put("value", tkw.trim());
+						tkws.add(tkmap);
+					}
+				}
+			}
+		}
+
 	}
 }
